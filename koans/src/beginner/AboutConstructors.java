8a6b4f78588d00cc95d9ccd6f817b215a144b095
Ceferino Villareal IV <clvillarealiv@gmail.com>
2020-05-23 18:33:39 +0800

    finish constructors

--- koans/src/beginner/AboutConstructors.java
+++ koans/src/beginner/AboutConstructors.java
@@ -1,6 +1,5 @@
 package beginner;
 
-
 import com.sandwich.koan.Koan;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
@@ -26,7 +25,7 @@ public class AboutConstructors {
 
     @Koan
     public void simpleConstructorOrder() {
-        assertEquals(new B().someString, __);
+        assertEquals(new B().someString, "axg");
     }
 
     class Aa {
@@ -51,7 +50,7 @@ public class AboutConstructors {
 
     @Koan
     public void complexConstructorOrder() {
-        assertEquals(new Bb().someString, __);
+        assertEquals(new Bb().someString, "aBoog");
     }
 
 }
